0dd34ff311fc96fe4eb3dec40bf378ee9af4bc52
taichanne30 <ne300209@senshu-u.jp>
2019-07-23 00:45:50 +0900

    最初のkeyDownを拾うように

--- scripts/input.js
+++ scripts/input.js
@@ -5,14 +5,39 @@ let lane_nums = {
     75: 4,
     79: 5
 }
+
+class pushed_state{
+    constructor(){
+        this._data = {};
+        Object.keys(lane_nums).forEach((inputCode) => {
+            this.setData(inputCode, false);
+        });
+    }
+    setData(key, value){
+        return this._data[key] = value;
+    };
+    getData(){
+        return this._data;
+    };
+    isPushed(code){
+        if(this._data[code] == undefined) return false;
+        return this._data[code];
+    };
+}
+let pState = new pushed_state();
+
+let ticking = false;
 $(document).keydown(function(event){
-    Object.entries(lane_nums).forEach(([inputCode, laneNum]) => {
-        if(event.keyCode == inputCode){
-            event.preventDefault();
-            $("#lane"+laneNum+":not(.pressed)").addClass("pressed");
-            return;
-        }
-    });
+    if(event.keyCode == 32) event.preventDefault();
+            //console.log("keydown \""+event.key +"\" : "+ event.keyCode);
+            Object.entries(lane_nums).forEach(([inputCode, laneNum]) => {
+                if(!pState.isPushed(event.keyCode) && event.keyCode == inputCode){
+                    console.log("first keydown of approved Key \""+event.key +"\" : "+ event.keyCode);
+                    pState.setData(event.keyCode, true);
+                    $("#lane"+laneNum+":not(.pressed)").addClass("pressed");
+                    return;
+                }
+            });
 });
 $(document).keyup(function(event){
     Object.entries(lane_nums).forEach(([inputCode, laneNum]) => {
@@ -20,5 +45,7 @@ $(document).keyup(function(event){
             $("#lane"+laneNum).removeClass("pressed");
             return;
         }
+        pState.setData(event.keyCode, false);
     });
+    console.log("keyup \""+event.key +"\" : "+ event.keyCode);
 });
